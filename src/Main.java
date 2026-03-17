981ed839a281a664ae34af2e9c98a90e5175ecb7
misha <orospu@MacBook-Pro-misha.local>
2026-03-17 14:23:02 +0100

    Dodanie pomocniczej informacji o aplikacji

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello from pgo-git-start");
+        System.out.println("This is feature/info branch.");
+    }
+}
